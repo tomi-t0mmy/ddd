f3302c509374a98e03129585b3091258fb356ee8
tomi-t0mmy <tomitomit0mmyyy@gmail.com>
2022-12-22 00:41:38 +0900

    不要なコード削除

--- server.go
+++ server.go
@@ -29,26 +29,3 @@ func createTodoHandler(r ITodoRepository) echo.HandlerFunc {
 		return c.String(http.StatusOK, TodoPresenter{}.TodoPresent(*todo))
 	}
 }
-
-// e.GET("/users/:id", getUser)
-func getUser(c echo.Context) error {
-	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
-
-//e.GET("/show", show)
-func show(c echo.Context) error {
-	// Get team and member from the query string
-	team := c.QueryParam("team")
-	member := c.QueryParam("member")
-	return c.String(http.StatusOK, "team:"+team+", member:"+member)
-}
-
-// e.POST("/save", save)
-func save(c echo.Context) error {
-	// Get name and email
-	name := c.FormValue("name")
-	email := c.FormValue("email")
-	return c.String(http.StatusOK, "name:"+name+", email:"+email)
-}
