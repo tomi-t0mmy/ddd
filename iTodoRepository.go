ddd3fb1ebd7920fce20a34c5213f1bc7041685f5
tomi-t0mmy <tomitomit0mmyyy@gmail.com>
2022-12-25 14:40:02 +0900

    id指定でtodoを取得できるようにする

--- iTodoRepository.go
+++ iTodoRepository.go
@@ -3,4 +3,5 @@ package main
 type ITodoRepository interface {
 	getAll() []Todo
 	createTodo(*Todo)
+	searchTodo(int) (*Todo, error)
 }
--- todoRepository.go
+++ todoRepository.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type InMemoryTodoRepository struct {
 	maxId int
 	todos []Todo
@@ -13,4 +15,13 @@ func (r *InMemoryTodoRepository) createTodo(t *Todo) {
 	t.Id = r.maxId + 1
 	r.maxId = r.maxId + 1
 	r.todos = append(r.todos, *t)
+}
+
+func (r *InMemoryTodoRepository) searchTodo(id int) (*Todo, error) {
+	for _, todo := range r.todos {
+		if todo.Id == id {
+			return &todo, nil
+		}
+	}
+	return nil, errors.New("error : The Todo doesn't exist")
 }
